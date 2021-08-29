51dee01392aad1010a2228cf93745253967d870a
David Duarte <davidandradeduarte@gmail.com>
2021-08-29 05:33:53 +0100

    3.1 - 3.4 ex

---
+++ exercises/3.1-4.go
@@ -0,0 +1,93 @@
+// Surface computes an SVG rendering of a 3-D surface function.
+package main
+
+import (
+	"fmt"
+	"io"
+	"log"
+	"math"
+	"net/http"
+)
+
+const (
+	width, height = 600, 320            // canvas size in pixels
+	cells         = 100                 // number of grid cells
+	xyrange       = 30.0                // axis ranges (-xyrange..+xyrange)
+	xyscale       = width / 2 / xyrange // pixels per x or y unit
+	zscale        = height * 0.4        // pixels per z unit
+	angle         = math.Pi / 6         // angle of x, y axes (=30°)
+)
+
+var sin30, cos30 = math.Sin(angle), math.Cos(angle) // sin(30°), cos(30°)
+
+type drawFunc func(x, y float64) float64
+
+var df drawFunc
+
+func main() {
+	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
+		w.Header().Set("Content-Type", "image/svg+xml")
+		df = eggbox
+		surface(w)
+	})
+	log.Fatal(http.ListenAndServe("localhost:8000", nil))
+}
+
+func surface(out io.Writer) {
+	svg := fmt.Sprintf("<svg xmlns='http://www.w3.org/2000/svg' "+
+		"style='stroke: grey; fill: white; stroke-width: 0.7' "+
+		"width='%d' height='%d'>", width, height)
+	for i := 0; i < cells; i++ {
+		for j := 0; j < cells; j++ {
+			ax, ay, _ := corner(i+1, j)
+			bx, by, _ := corner(i, j)
+			cx, cy, _ := corner(i, j+1)
+			dx, dy, p := corner(i+1, j+1)
+			var color string
+			if p {
+				color = "#ff0000"
+			} else {
+				color = "#0000ff"
+			}
+			// not sure if we should be using math.IsNaN or math.IsInf
+			if math.IsNaN(ax) || math.IsNaN(ay) || math.IsNaN(bx) || math.IsNaN(by) || math.IsNaN(cx) || math.IsNaN(cy) || math.IsNaN(dx) || math.IsNaN(dy) {
+				continue
+			}
+			svg += fmt.Sprintf("<polygon style='fill: %s' points='%g,%g %g,%g %g,%g %g,%g'/>\n",
+				color, ax, ay, bx, by, cx, cy, dx, dy)
+		}
+	}
+	svg += "</svg>"
+	out.Write([]byte(svg))
+}
+
+func corner(i, j int) (float64, float64, bool) {
+	// Find point (x,y) at corner of cell (i,j).
+	x := xyrange * (float64(i)/cells - 0.5)
+	y := xyrange * (float64(j)/cells - 0.5)
+
+	// Compute surface height z.
+	z := df(x, y)
+
+	// Project (x,y,z) isometrically onto 2-D SVG canvas (sx,sy).
+	sx := width/2 + (x-y)*cos30*xyscale
+	sy := height/2 + (x+y)*sin30*xyscale - z*zscale
+	return sx, sy, z >= 0
+}
+
+func f(x, y float64) float64 {
+	h := math.Hypot(x, y) // distance from (0,0)
+	return math.Sin(h) / h
+}
+
+func eggbox(x, y float64) float64 {
+	return 0.2 * (math.Cos(x) + math.Cos(y))
+}
+
+func saddle(x, y float64) float64 {
+	a := 25.0
+	b := 17.0
+	a2 := a * a
+	b2 := b * b
+	return (y*y/a2 - x*x/b2)
+}
