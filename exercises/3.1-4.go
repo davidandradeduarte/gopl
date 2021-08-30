2e1d5e488dd7be366d061274593a81fc49e1fd9c
David Duarte <davidandradeduarte@gmail.com>
2021-08-30 14:44:23 +0100

    3.5

--- exercises/3.1-4.go
+++ exercises/3.1-4.go
@@ -27,7 +27,7 @@ var df drawFunc
 func main() {
 	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
 		w.Header().Set("Content-Type", "image/svg+xml")
-		df = eggbox
+		df = f
 		surface(w)
 	})
 	log.Fatal(http.ListenAndServe("localhost:8000", nil))
